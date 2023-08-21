98b9a5080ede1f4e06ff525ba984d2a0df7249fd
xian1522 <xian1522@126.com>
2023-08-22 00:32:43 +0800

    个人中心头像上传

--- src/api/system/user.js
+++ src/api/system/user.js
@@ -116,3 +116,13 @@ export function deptTreeSelect(){
         method: 'get'
     })
 }
+
+
+// 用户头像上传
+export function uploadAvatar(data) {
+    return request({
+      url: '/system/user/profile/avatar',
+      method: 'post',
+      data: data
+    })
+  }
--- src/views/system/user/profile/index.vue
+++ src/views/system/user/profile/index.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>个人中心</div>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :span="6" :xs="24">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>个人信息</span>
+          </div>
+          <div class="text-center">
+            <user-avatar :user="user"/>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import userAvatar from './userAvatar.vue'
+import { getUserProfile } from '@/api/system/user'
 export default {
+  components: { userAvatar },
   name: 'Profile',
+  data() {
+    return {
+      user: {},
+    }
+  },
+  created() {
+    this.getUser()
+  },
+  methods: {
+    getUser() {
+      getUserProfile().then(response => {
+        this.user = response.data
+      })
+    },
+  }
 }
 </script>
 
